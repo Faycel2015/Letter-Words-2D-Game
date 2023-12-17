80fdaf781c64251dbd32b31a3ecac19d1e403fbf
Faycel2015 <faycel.ayech@gmail.com>
2023-12-18 00:05:22 +0100

    Update Scripts

--- Assets/Words Game/Scripts/HintManager.cs
+++ Assets/Words Game/Scripts/HintManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class HintManager : MonoBehaviour
 {
+    [Header(" Elements ")]
+    [SerializeField] private GameObject keyboard;
+    private KeyboardKey[] keys;
+
+    private void Awake()
+    {
+        keys = keyboard.GetComponentsInChildren<KeyboardKey>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,33 @@ public class HintManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void KeyboardHint()
+    {
+        string secretWord = WordManager.instance.GetSecretWord();
+
+        List<KeyboardKey> untouchedKeys = new List<KeyboardKey>();
+
+        for (int i = 0; i < keys.Length; i++)
+            if (keys[i].IsUntouched())
+                untouchedKeys.Add(keys[i]);
+
+        List<KeyboardKey> t_untouchedKeys = new List<KeyboardKey>(untouchedKeys);
+
+        for (int i = 0; i < untouchedKeys.Count; i++)
+            if (secretWord.Contains(untouchedKeys[i].GetLetter()))
+                t_untouchedKeys.Remove(untouchedKeys[i]);
+
+        if (t_untouchedKeys.Count <= 0)
+            return;
+
+        int randomKeyIndex = Random.Range(0, t_untouchedKeys.Count);
+        t_untouchedKeys[randomKeyIndex].SetInvalid();
+    }
+
+    public void LetterHint()
+    {
+
     }
 }
--- Assets/Words Game/Scripts/KeyboardKey.cs
+++ Assets/Words Game/Scripts/KeyboardKey.cs
@@ -71,4 +71,9 @@ public class KeyboardKey : MonoBehaviour
         renderer.color = Color.gray;
         validity = Validity.Invalid;
     }
+
+    public bool IsUntouched()
+    {
+        return validity == Validity.None;
+    }
 }
